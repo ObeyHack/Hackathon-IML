4a575885e52dae40adef48a2112a01ce3481ea95
sean.cherny <sean.cherny@mail.huji.ac.il>
2023-06-07 21:34:01 +0300

    base

--- Preprocess_data.py
+++ Preprocess_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+
+CATGEORIAL
 
 def preprocess():
     # Read data
--- currency_convert.py
+++ currency_convert.py
@@ -2,6 +2,6 @@ from currency_converter import CurrencyConverter
 # pip install --user currencyconverter
 
 
-def to_dollar(amount_of_money : float, original_currency : str):
+def to_dollar(amount_of_money : float, original_currency: str):
     c = CurrencyConverter()
     return c.convert(amount_of_money, original_currency, "USD")
