7ae65863bf4a4819656efe525b1b48eb158df9ae
michaelmerzin <merzin03@gmail.com>
2023-06-07 21:00:17 +0300

    basic preprocess data

---
+++ Preprocess_data.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+def preprocess():
+    # Read data
+    data = pd.read_csv('data\\agoda_cancellation_train.csv')
+    # Split the data into X and y
+    X_train = data.drop('cancellation_datetime', axis=1)
+    y_train = data['cancellation_datetime']
+    # binary the y_train
+    y_train = y_train.apply(lambda x: 0 if pd.isnull(x) else 1)
+    #make date to datetime
+    X_train['checkin_date'] = pd.to_datetime(X_train['checkin_date'])
+    X_train['checkout_date'] = pd.to_datetime(X_train['checkout_date'])
+    X_train['booking_datetime'] = pd.to_datetime(X_train['checkout_date'])
+    return X_train, y_train
+
+
+def main():
+    preprocess()
+if __name__ == '__main__':
+    main()
