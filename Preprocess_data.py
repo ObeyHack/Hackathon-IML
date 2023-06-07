9b5432276d3ee367f936de6292bc7352be258106
sean.cherny <sean.cherny@mail.huji.ac.il>
2023-06-07 21:36:00 +0300

    base

--- Preprocess_data.py
+++ Preprocess_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-CATGEORIAL
+CATEGORICAL_FEATURES = ["charge_option", "customer_nationality", "guest_nationality_country_name", "origin_country_code"
+                       "language", "original_payment_method", "original_payment_type"]
 
 def preprocess():
     # Read data
@@ -10,6 +11,11 @@ def preprocess():
     y_train = data['cancellation_datetime']
     # binary the y_train
     y_train = y_train.apply(lambda x: 0 if pd.isnull(x) else 1)
+
+
+
+
+
     # make date to datetime
     X_train['checkin_date'] = pd.to_datetime(X_train['checkin_date'])
     X_train['checkout_date'] = pd.to_datetime(X_train['checkout_date'])
