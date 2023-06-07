24d831590ecbeb23f4300c93487e4cb4ec68cf3b
ofir rosenstock <ofir.rosenstock@mail.huji.ac.il>
2023-06-07 21:30:54 +0300

    added "to_dollar()" function

---
+++ currency_convert.py
@@ -0,0 +1,7 @@
+from currency_converter import CurrencyConverter
+# pip install --user currencyconverter
+
+
+def to_dollar(amount_of_money : float, original_currency : str):
+    c = CurrencyConverter()
+    return c.convert(amount_of_money, original_currency, "USD")
